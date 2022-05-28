b65fd2c25cffcc0736c9e2a41333b664ff4ded97
myishatunsamiha <myisha.samiha@gmail.com>
2022-05-28 21:51:45 +0600

    Added portfolio.

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ import MakeAdmin from './pages/Dashboard/MakeAdmin';
 import ManageProducts from './pages/Dashboard/ManageProducts';
 import Payment from './pages/Dashboard/Payment';
 import NotFound from './pages/NotFound/NotFound';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Blogs from './pages/Blogs/Blogs';
 
 
 function App() {
@@ -33,6 +35,8 @@ function App() {
         <Route path="/" element={<Home />} ></Route>
         <Route path="login" element={<Login />} ></Route>
         <Route path="signup" element={<Signup />} ></Route>
+        <Route path="blogs" element={<Blogs />} ></Route>
+        <Route path="portfolio" element={<Portfolio />} ></Route>
         <Route path="purchase/:id" element={<RequireAuth><Purchase /></RequireAuth>} />
         <Route path="dashboard" element={<RequireAuth><Dashboard></Dashboard></RequireAuth>}>
           <Route index element={<MyProfile></MyProfile>}></Route>
---
+++ src/pages/Portfolio/Portfolio.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div className='flex flex-col justify-center items-center p-24'>
+
+            <p className='text-red-800 text-2xl mb-3'>Name: <span className='text-gray-800 font-bold'>Myisha Tun Samiha</span></p>
+            <p className='text-red-800 text-2xl mb-3'>Email: <span className='text-gray-800 font-bold'>myisha.samiha@gmail.com</span></p>
+            <p className='text-red-800 text-2xl mb-3'>Education: <span className='text-gray-800 font-bold'>BSc. in Computer Science and Engineering</span></p>
+            <p className='text-red-800 text-2xl mb-3'>Web Development Languages: <span className='text-gray-800 font-bold'>HTML, CSS, JavaScript, Python, PHP</span></p>
+            <p className='text-red-800 text-2xl mb-3'>Web Development Frameworks/Libraries: <span className='text-gray-800 font-bold'>BootStrap, Tailwind, React, NodeJs, Express, Django</span></p>
+            <p className='text-red-800 text-2xl mb-3'>Web Development Databases: <span className='text-gray-800 font-bold'>MongoDB, SQLite, SQL</span></p>
+            <p className='text-red-800 text-2xl mb-3'>Project-1 link: <span className='text-gray-800 font-bold'>https://stationary-corner-myishatunsamiha.netlify.app/</span></p>
+            <p className='text-red-800 text-2xl mb-3'>Project-2 link: <span className='text-gray-800 font-bold'>https://excel-tuition.web.app/</span></p>
+            <p className='text-red-800 text-2xl mb-3'>Project-3 link: <span className='text-gray-800 font-bold'>https://restaurantreviewzone.netlify.app</span></p>
+
+        </div>
+    );
+};
+
+export default Portfolio;
